9a6a262cc13430b515ab198ae0c0452e5403795a
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-11 22:35:42 +0100

    Interact with SimpleStorage

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -23,6 +23,19 @@ async function main() {
         // Verify simpleStorage contract with no constructors
         await verify(simpleStorage.address, []);
     }
+
+    // Interacting with simple storage contract
+    // Get initial favorite number
+    const initialFavoriteNumber = await simpleStorage.retrieve();
+    console.log(`Initial favorite number: ${initialFavoriteNumber}`);
+
+    // Update favorite number
+    const transactionResponse = await simpleStorage.store(15);
+    await transactionResponse.wait(1);
+
+    // Get updated favorite number
+    const updatedFavoriteNumber = await simpleStorage.retrieve();
+    console.log(`Updated favorite number: ${updatedFavoriteNumber}`);
 }
 
 // Async function to automatically verify deployed contract
