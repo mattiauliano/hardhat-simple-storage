e560cad8db45d6c0cadb4095a1a57728cb5bafb6
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-11 02:40:44 +0100

    Compile SimpleStorage.sol

---
+++ test/SimpleStorage.js
@@ -0,0 +1,6 @@
+const {
+    time,
+    loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
