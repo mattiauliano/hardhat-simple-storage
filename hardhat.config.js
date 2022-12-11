2435c822efebe409b85144b0946e5c1e5688e50d
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-11 03:38:11 +0100

    Add goerli test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+// Import dotenv to get access to .env
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+// Get rpc url and private key to use in goerli network
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
+    // Default local fake network
+    defaultNetwork: "hardhat",
+    // Adding external network
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
     solidity: "0.8.7",
 };
