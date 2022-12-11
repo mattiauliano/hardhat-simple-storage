1a02da6d174a902d3fc0be816630b2a53caf5fcd
Mattia Uliano <mattiauliano308@gmail.com>
2022-12-11 21:35:41 +0100

    Add etherscan api

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 // Import dotenv to get access to .env
 require("dotenv").config();
+// Import hardhat-ethers to verify contracts
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 // Get rpc url and private key to use in goerli network
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
     // Default local fake network
@@ -18,6 +21,10 @@ module.exports = {
             accounts: [PRIVATE_KEY],
             chainId: 5,
         },
+    },
+    // Adding a new command "verify"
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
     },
     solidity: "0.8.7",
 };
